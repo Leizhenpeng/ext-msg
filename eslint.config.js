2866359e7df014b5db841cd029272e2587663999
river <laolei@forkway.cn>
2024-07-31 10:16:47 +0800

    chore: disable console errors in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,16 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     ignores: [
-      // eslint ignore globs here
+      // eslint ignore globs here、
+      // console off
+
     ],
   },
   {
     rules: {
       // overrides
+      'no-console': 'off', // 关闭 console 错误
+
     },
   },
 )
